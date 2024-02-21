674a102694cad63f114f99611ac32234e44b35cd
Semra.Pelfanova <Semra.Pelfanova@LTDIGI011>
2024-02-21 15:56:49 +0200

    check exisiting product size and group

---
+++ ProductWarehouse.Application/Features/Commands/Products/Group/CreateProductGroup/CreateProductGroupCommandValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using ProductWarehouse.Application.Constants;
+using ProductWarehouse.Application.Interfaces;
+
+namespace ProductWarehouse.Application.Features.Commands.Products.Group.CreateProductGroup;
+public class CreateProductGroupCommandValidation : AbstractValidator<CreateProductGroupCommand>
+{
+	private readonly IUnitOfWork _unitOfWork;
+
+	public CreateProductGroupCommandValidation(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+
+		RuleFor(command => command.GroupId)
+			.NotEmpty()
+			.WithMessage(MessageConstants.RequiredValidationMessage(nameof(CreateProductGroupCommand.GroupId)))
+			.MustAsync((command, sizeId, cancellationToken) => ProductSizeExists(command.ProductId, sizeId, cancellationToken))
+			.WithMessage(MessageConstants.AlreadyExistMessage(nameof(CreateProductGroupCommand.GroupId)));
+	}
+
+	private async Task<bool> ProductSizeExists(Guid productId, Guid groupId, CancellationToken cancellationToken)
+	{
+		var product = await _unitOfWork.Products.GetProductDetailsAsync(productId);
+
+		if (product != null)
+		{
+			return !product.ProductGroups.Any(x => x.GroupId == groupId);
+		}
+		return product is null;
+	}
+}
